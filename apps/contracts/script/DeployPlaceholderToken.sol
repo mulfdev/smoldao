053e87f7a8dfb174d1043c1d19902dea709353cb
mulf <mail@mulf.wtf>
2024-10-11 18:25:38 -0500

    new script

--- apps/contracts/script/DeployPlaceholderToken.sol
+++ apps/contracts/script/DeployPlaceholderToken.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.25;
 
-import { Foo } from "../src/Foo.sol";
+import { PlaceholderToken } from "../src/PlaceholderToken.sol";
 
 import { BaseScript } from "./Base.s.sol";
 
 /// @dev See the Solidity Scripting tutorial: https://book.getfoundry.sh/tutorials/solidity-scripting
 contract Deploy is BaseScript {
-    function run() public broadcast returns (Foo foo) {
-        foo = new Foo();
+    function run() public broadcast returns (PlaceholderToken plt) {
+        plt = new PlaceholderToken();
     }
 }
