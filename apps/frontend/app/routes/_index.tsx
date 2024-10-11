cc7098330d7e86db7dfc54d4e3079893ce298e26
mulfdev <mulfdev@users.noreply.github.com>
2024-10-11 19:26:33 +0000

    style: format frontend code with pnpm format

--- apps/frontend/app/routes/_index.tsx
+++ apps/frontend/app/routes/_index.tsx
@@ -13,7 +13,6 @@ export default function Index() {
     <div className="flex h-screen items-center justify-center">
       <div className="flex flex-col items-center gap-16">
         <header className="flex flex-col items-center gap-9">
-
           <h1 className="leading text-2xl font-bold text-gray-800 dark:text-gray-100">
             Welcome to <span className="sr-only">Remix</span>
           </h1>
@@ -31,7 +30,6 @@ export default function Index() {
           </div>
         </header>
         <nav className="flex flex-col items-center justify-center gap-4 rounded-3xl border border-gray-200 p-6 dark:border-gray-700">
-
           <ConnectKitButton />
           <ConnectKitButton />
           <ConnectKitButton />
