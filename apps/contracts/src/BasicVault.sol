a9114d2aab900c65bccee41c13016ba9e71d365f
mulf <mail@mulf.wtf>
2024-10-12 16:25:39 -0500

    replace mint and burn funcs with token funcs

--- apps/contracts/src/BasicVault.sol
+++ apps/contracts/src/BasicVault.sol
@@ -1,26 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { SmolGov } from "./SmolGov.sol";
 
 contract Vault {
-    IERC20 public immutable token;
+    SmolGov public immutable token;
 
     uint256 public totalSupply;
-    mapping(address => uint256) public balanceOf;
 
     constructor(address _token) {
-        token = IERC20(_token);
-    }
-
-    function _mint(address _to, uint256 _shares) private {
-        totalSupply += _shares;
-        balanceOf[_to] += _shares;
-    }
-
-    function _burn(address _from, uint256 _shares) private {
-        totalSupply -= _shares;
-        balanceOf[_from] -= _shares;
+        token = SmolGov(_token);
     }
 
     function deposit(uint256 _amount) external {
@@ -41,8 +30,7 @@ contract Vault {
             shares = (_amount * totalSupply) / token.balanceOf(address(this));
         }
 
-        _mint(msg.sender, shares);
-        token.transferFrom(msg.sender, address(this), _amount);
+        token.mint(msg.sender, shares);
     }
 
     function withdraw(uint256 _shares) external {
@@ -57,7 +45,6 @@ contract Vault {
         a = sB / T
         */
         uint256 amount = (_shares * token.balanceOf(address(this))) / totalSupply;
-        _burn(msg.sender, _shares);
-        token.transfer(msg.sender, amount);
+        token.burn(amount);
     }
 }
