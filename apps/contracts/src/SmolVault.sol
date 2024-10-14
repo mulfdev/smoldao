fa92b991ad7edcbc03cf3dcd46b45033f3ac6264
mulf <mail@mulf.wtf>
2024-10-13 22:20:00 -0500

    fix calculation

--- apps/contracts/src/SmolVault.sol
+++ apps/contracts/src/SmolVault.sol
@@ -5,7 +5,6 @@ import { SmolGov } from "./SmolGov.sol";
 
 contract SmolVault {
     SmolGov public immutable token;
-
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
 
@@ -24,39 +23,21 @@ contract SmolVault {
     }
 
     function deposit(uint256 _amount) external {
-        /*
-        a = amount
-        B = balance of token before deposit
-        T = total supply
-        s = shares to mint
-
-        (T + s) / T = (a + B) / B 
-
-        s = aT / B
-        */
+        require(_amount > 0, "Deposit amount must be greater than 0");
         uint256 shares;
         if (totalSupply == 0) {
             shares = _amount;
         } else {
-            shares = (_amount * totalSupply) / token.balanceOf(address(this));
+            shares = (_amount * totalSupply) / token.balanceOf(msg.sender);
         }
-
         _mint(msg.sender, shares);
-        token.mint(msg.sender, shares);
+        token.mint(msg.sender, _amount);
     }
 
     function withdraw(uint256 _shares) external {
-        /*
-        a = amount
-        B = balance of token before withdraw
-        T = total supply
-        s = shares to burn
-
-        (T - s) / T = (B - a) / B 
-
-        a = sB / T
-        */
-        uint256 amount = (_shares * token.balanceOf(address(this))) / totalSupply;
+        require(_shares > 0, "Withdraw amount must be greater than 0");
+        require(balanceOf[msg.sender] >= _shares, "Insufficient balance");
+        uint256 amount = (_shares * token.balanceOf(msg.sender)) / totalSupply;
         _burn(msg.sender, _shares);
         token.burn(amount);
     }
