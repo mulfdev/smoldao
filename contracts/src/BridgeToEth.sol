38d9ae968ee75c0641b778c642792986b75501b7
mulf <mail@mulf.wtf>
2024-10-24 18:49:52 -0500

    bridge to eth from arb

---
+++ contracts/src/BridgeToEth.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import {OApp, Origin, MessagingFee} from "@layerzerolabs/oapp-evm/contracts/oapp/OApp.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+contract BridgeToEth is OApp, ERC1155 {
+    uint256 public constant TOKEN_ID = 1;
+
+    event TokensMinted(address to, uint256 amount);
+
+    constructor(address _endpoint, address _owner, string memory _uri)
+        OApp(_endpoint, _owner)
+        Ownable(_owner)
+        ERC1155(_uri)
+    {}
+
+    function _lzReceive(Origin calldata, bytes32, bytes calldata payload, address, bytes calldata) internal override {
+        (address to, uint256 amount) = abi.decode(payload, (address, uint256));
+
+        _mint(to, TOKEN_ID, amount, "");
+
+        emit TokensMinted(to, amount);
+    }
+}
