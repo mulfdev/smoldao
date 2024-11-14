11bb503e5ebe90d57f349f677a503d8095210cd8
mulf <mail@mulf.wtf>
2024-11-14 17:21:28 -0600

    switch to arb

--- frontend/app/wagmiConfig.ts
+++ frontend/app/wagmiConfig.ts
@@ -1,15 +1,17 @@
 import { createConfig, http } from "wagmi";
-import { mainnet } from "wagmi/chains";
+import { arbitrumSepolia } from "wagmi/chains";
 import { getDefaultConfig } from "connectkit";
 
 export function setupWagmi(RPC_URL: string, WALLETCONNECT_PROJECT_ID: string) {
   const config = createConfig(
     getDefaultConfig({
       // Your dApps chains
-      chains: [mainnet],
+      chains: [
+        arbitrumSepolia
+      ],
       transports: {
         // RPC URL for each chain
-        [mainnet.id]: http(RPC_URL),
+        [arbitrumSepolia.id]: http(RPC_URL),
       },
       ssr: true,
 
