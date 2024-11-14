da8f4cf7c31ea0d369bf722ee419c885a97723d1
mulf <mail@mulf.wtf>
2024-11-13 18:25:07 -0600

    vault deposit script to match new logic

--- contracts/script/VaultDeposit.s.sol
+++ contracts/script/VaultDeposit.s.sol
@@ -6,35 +6,34 @@ import {SmolVault} from "../src/SmolVault.sol";
 import {SmolGov} from "../src/SmolGov.sol";
 import "forge-std/console.sol";
 
+import {IERC1155} from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+
 contract VaultDeposit is BaseScript {
     function run() external broadcaster {
-        string memory json = vm.readFile("deployed_addresses.json");
-        address vaultAddress = abi.decode(vm.parseJson(json, ".SmolVault"), (address));
-
-        SmolVault vault = SmolVault(vaultAddress);
-        SmolGov token = vault.token();
+        SmolVault vault = SmolVault(0x39356f78560b0DCFE6Bd816ac68a285C2a567bb5);
+        SmolGov token = vault.governanceToken();
 
         console.log("Deployer address:", deployer);
         console.log("Vault address:", address(vault));
         console.log("Token address:", address(token));
 
-        uint256 depositAmount = 2000e18;
+        uint256 depositAmount = 5;
         console.log("Attempting to deposit:", depositAmount);
 
-        uint256 initialVaultBalance = vault.balanceOf(deployer);
+        uint256 initialVaultBalance = token.balanceOf(deployer);
         uint256 initialTokenBalance = token.balanceOf(deployer);
         console.log("Initial vault shares balance:", initialVaultBalance);
         console.log("Initial user token balance:", initialTokenBalance);
 
-        try vault.deposit(depositAmount) {
-            console.log("Deposit successful");
-            uint256 newVaultBalance = vault.balanceOf(deployer);
-            uint256 newTokenBalance = token.balanceOf(deployer);
-            console.log("New vault shares balance:", newVaultBalance);
-            console.log("New user token balance:", newTokenBalance);
-            console.log("Shares minted:", newVaultBalance - initialVaultBalance);
-            console.log("Tokens minted:", newTokenBalance - initialTokenBalance);
+        IERC1155(0x1e38AFBb2628943ABbDCDAb7151Fa84990930F4C).setApprovalForAll(
+            address(vault),
+            true
+        );
 
+        try vault.deposit(1, depositAmount) {
+            console.log("Deposit successful");
+            uint256 newTokenBalance = token.balanceOf(deployer);
+            console.log("New user token balance:", newTokenBalance);
             // Delegate votes to self
             token.delegate(deployer);
             console.log("Votes delegated to self");
@@ -45,7 +44,7 @@ contract VaultDeposit is BaseScript {
             console.logBytes(lowLevelData);
         }
 
-        uint256 vaultTotalSupply = vault.totalSupply();
+        uint256 vaultTotalSupply = token.totalSupply();
         console.log("Vault total supply:", vaultTotalSupply);
         address tokenOwner = token.owner();
         console.log("Token owner:", tokenOwner);
