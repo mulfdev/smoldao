9677e10d5a6da3fb958dca7f4679be3b41d0a883
mulf <mail@mulf.wtf>
2024-11-13 18:26:11 -0600

    burnFrom

--- contracts/src/SmolGov.sol
+++ contracts/src/SmolGov.sol
@@ -10,17 +10,51 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 contract SmolGov is ERC20, ERC20Permit, ERC20Votes, ERC20Burnable, Ownable {
-    constructor(address initialOwner) ERC20("SmolGov", "SmolG") Ownable(initialOwner) ERC20Permit("SmolGov") {}
+    constructor(
+        address initialOwner
+    ) ERC20("SmolGov", "SmolG") Ownable(initialOwner) ERC20Permit("SmolGov") {}
+
+    function transfer(
+        address to,
+        uint256 amount
+    ) public virtual override onlyOwner returns (bool) {
+        return super.transfer(to, amount);
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) public virtual override onlyOwner returns (bool) {
+        return super.transferFrom(from, to, amount);
+    }
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
 
-    function _update(address from, address to, uint256 amount) internal override(ERC20, ERC20Votes) {
+    function burn(uint256 amount) public virtual override onlyOwner {
+        super.burn(amount);
+    }
+
+    function burnFrom(
+        address account,
+        uint256 amount
+    ) public virtual override onlyOwner {
+        super.burnFrom(account, amount);
+    }
+
+    function _update(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override(ERC20, ERC20Votes) {
         super._update(from, to, amount);
     }
 
-    function nonces(address owner) public view virtual override(ERC20Permit, Nonces) returns (uint256) {
+    function nonces(
+        address owner
+    ) public view virtual override(ERC20Permit, Nonces) returns (uint256) {
         return super.nonces(owner);
     }
 }
