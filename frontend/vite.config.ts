9a8ad6100fb916674e884dedbc73862d22279e2a
mulf <mail@mulf.wtf>
2024-11-14 20:58:52 -0600

    load env in prod

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,24 @@
 import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-  plugins: [
-    remix({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-      },
-    }),
-    tsconfigPaths(),
-  ],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [
+      remix({
+        future: {
+          v3_fetcherPersist: true,
+          v3_relativeSplatPath: true,
+          v3_throwAbortReason: true,
+        },
+      }),
+      tsconfigPaths(),
+    ],
+    define: {
+      "process.env.WALLETCONNECT_PROJECT_ID": JSON.stringify(process.env.WALLETCONNECT_PROJECT_ID || env.WALLETCONNECT_PROJECT_ID),
+      "process.env.RPC_URL": JSON.stringify(process.env.RPC_URL || env.RPC_URL),
+    }
+  };
 });
