3038bd771e12c03b49c3d8776ff795f26e81e2dc
mulf <mail@mulf.wtf>
2024-11-20 15:45:33 -0600

    rename token

--- contracts/src/Smol.sol
+++ contracts/src/Smol.sol
@@ -1,26 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-// This is the new token on Ethereum
-
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import {Nonces} from "@openzeppelin/contracts/utils/Nonces.sol";
 import {OFT} from "@layerzerolabs/oft-evm/contracts/OFT.sol";
+import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-
-contract SmolGov is ERC20, ERC20Permit, ERC20Votes, ERC20Burnable, Ownable {
+contract Smol is ERC20Permit, ERC20Votes, ERC20Burnable, OFT {
     constructor(
-        address initialOwner
-    ) ERC20("SmolGov", "SmolG") Ownable(initialOwner) ERC20Permit("SmolGov") {}
+        address initialOwner,
+        address endpoint
+    )
+        OFT("Smol", "Smol", endpoint, initialOwner)
+        Ownable(initialOwner)
+        ERC20Permit("Smol")
+    {}
 
     function transfer(
         address to,
         uint256 amount
-    ) public virtual override onlyOwner returns (bool) {
+    ) public virtual override(ERC20) onlyOwner returns (bool) {
         return super.transfer(to, amount);
     }
 
@@ -28,23 +30,12 @@ contract SmolGov is ERC20, ERC20Permit, ERC20Votes, ERC20Burnable, Ownable {
         address from,
         address to,
         uint256 amount
-    ) public virtual override onlyOwner returns (bool) {
+    ) public virtual override(ERC20) onlyOwner returns (bool) {
         return super.transferFrom(from, to, amount);
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
-    }
-
-    function burn(uint256 amount) public virtual override onlyOwner {
-        super.burn(amount);
-    }
-
-    function burnFrom(
-        address account,
-        uint256 amount
-    ) public virtual override onlyOwner {
-        super.burnFrom(account, amount);
     }
 
     function _update(
@@ -59,5 +50,16 @@ contract SmolGov is ERC20, ERC20Permit, ERC20Votes, ERC20Burnable, Ownable {
         address owner
     ) public view virtual override(ERC20Permit, Nonces) returns (uint256) {
         return super.nonces(owner);
+    }
+
+    function burn(uint256 amount) public virtual override onlyOwner {
+        super.burn(amount);
+    }
+
+    function burnFrom(
+        address account,
+        uint256 amount
+    ) public virtual override onlyOwner {
+        super.burnFrom(account, amount);
     }
 }
