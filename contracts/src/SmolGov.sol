fd79a01ee6cfaf6ab9e930db049f4a7f4b66ea2c
mulf <mail@mulf.wtf>
2024-11-19 18:11:19 -0600

    misc

--- contracts/src/SmolGov.sol
+++ contracts/src/SmolGov.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+// This is the new token on Ethereum
+
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
