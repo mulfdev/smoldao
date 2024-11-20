33cc2ac9b9084f411083224d961bf873516f2bc4
mulf <mail@mulf.wtf>
2024-11-20 12:33:52 -0600

    checkpoint

--- contracts/src/SmolAdapter.sol
+++ contracts/src/SmolAdapter.sol
@@ -1,10 +1,8 @@
-// SPDX-License-Identifier: MIT 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
-pragma solidity ^0.8.20
-
-
-import { OFTAdapter } from "@layerzerolabs/oft-evm/contracts/OFTAdapter.sol";
-import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import {OFTAdapter} from "@layerzerolabs/oft-evm/contracts/OFTAdapter.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 /// @notice OFTAdapter uses a deployed ERC-20 token and safeERC20 to interact with the OFTCore contract.
 contract MyOFTAdapter is OFTAdapter {
@@ -12,4 +10,5 @@ contract MyOFTAdapter is OFTAdapter {
         address _token,
         address _lzEndpoint,
         address _owner
-    ) OFTAdapter(_token, _layerZeroEndpoint, _owner) Ownable(_owner) {}
+    ) OFTAdapter(_token, _lzEndpoint, _owner) Ownable(_owner) {}
+}
