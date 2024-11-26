d614ca5a112b5a91d5cd11e6ee70ed825b6ae2bc
mulf <mail@mulf.wtf>
2024-11-26 11:31:32 -0600

    client loader

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,60 +1,47 @@
 import type { MetaFunction } from "@remix-run/node";
-import { ConnectKitButton } from "connectkit";
-import { Suspense } from "react";
+import { useLoaderData } from "@remix-run/react";
 
-import { gql, useQuery } from "urql";
+import { gql } from "@urql/core";
+
 import ProposalInfo from "~/components/ViewProposals";
+import { gqlClient } from "~/gqlConfig";
 
 const PropsQuery = gql`
-  query {
-    proposalCreateds {
-      proposer
-      proposalId
-      voteEnd
-      voteStart
-      description
-      targets
-      block_number
-      values
-      calldatas
-      timestamp_
+    query {
+        proposalCreateds {
+            proposer
+            proposalId
+            voteEnd
+            voteStart
+            description
+            targets
+            block_number
+            values
+            calldatas
+            timestamp_
+        }
     }
-  }
 `;
 
-
-function Loading() {
-  return <>Loading</>
-}
-
 export const meta: MetaFunction = () => {
-  return [
-    { title: "SmolDAO" },
-    { name: "description", content: "Smolest DAO around" },
-  ];
+    return [{ title: "SmolDAO" }, { name: "description", content: "Smolest DAO around" }];
 };
 
+export async function clientLoader() {
+    const result = await gqlClient.query(PropsQuery, {}).toPromise();
+    return result;
+}
+
 export default function Index() {
-  const [result] = useQuery({
-    query: PropsQuery,
-  });
-
-  const { data, fetching, error } = result;
-
-  if (fetching) return <p>Loading...</p>;
-  if (error) return <p>Oh no... {error.message}</p>;
-
-
-  return (
-    <>
-      <header>
-        <h1 className="leading text-4xl font-bold text-gray-800 dark:text-gray-100">
-          Welcome to SmolDAO
-        </h1>
-      </header>
-      <Suspense fallback={<Loading />}>
-        <ProposalInfo proposals={data.proposalCreateds} />
-      </Suspense>
-    </>
-  );
+    const query = useLoaderData<typeof clientLoader>();
+    return (
+        <>
+            <header>
+                <h1 className="leading text-4xl font-bold text-gray-800 dark:text-gray-100">
+                    Welcome to SmolDAO
+                </h1>
+            </header>
+            <ProposalInfo proposals={query.data.proposalCreateds} />
+        </>
+    );
 }
